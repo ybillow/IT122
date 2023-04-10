27a7f389345b9cda08bd44ac48b82eabc6316bb2
Yonus Billow <98870419+ybillow@users.noreply.github.com>
2023-04-09 22:02:54 -0700

    added change

---
+++ script.js
@@ -0,0 +1,5 @@
+// Define variables for firstName, lastName, yearsOfStudy, and goal
+let firstName = "Yonus";
+let lastName = "Billow";
+let yearsOfStudy = 1;
+let goal = "to become a software engineer";
